e807f59bb5aa8a362c3cca16081b72b833cb4e8e
Kobe A <andayakobe@gmail.com>
2023-09-29 11:21:25 -0700

    Initial Commit

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Circle : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = UnityEngine.Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if(UnityEngine.Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
+    }
+
+ 
+}
